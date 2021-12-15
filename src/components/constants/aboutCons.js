6028c4467bc414930b227fc41e4109ddd7ff49b4
boy2balls <nguyendanghoaa@gmail.com>
2021-12-15 09:24:23 +0700

    vision and mission

--- src/components/constants/aboutCons.js
+++ src/components/constants/aboutCons.js
@@ -47,12 +47,12 @@ export const aboutCons = {
     vision: {
       heading: "Vision",
       content:
-        "In the context of Vietnam expanding foreign trade activities, economic exchanges with other countries around the world, companies providing services today often only provide a certain segment, typically, law firms, including large companies, often have only strengths in legal advice with little practical experience in import and export procedures and post-customs clearance material management.",
+        "베트남은 해외무역과 경제교류를 확대하고 있습니다. 이에 국제적인 관세이슈로서 FTA 원산지, 비관세장벽, 덤핑관세, 세이프가드 등 다양한 이슈들이 발생하고 있습니다.이러한 이슈들은 수출입통관 실무와 연계되어 있습니다. 따라서, 회계법인, 법무법인에서 정확한 자문이 어렵습니다. 저희 UNI에서는 관련 규정과 함께 실무 관행까지 검토하여 정확한 관세 자문을 제공하고 있습니다.",
     },
     ourMission: {
       heading: "Our Mission",
       content:
-        'On the other hand, logistics companies, despite their extensive experience, often lack an in-depth team to advise on the problems that import-export enterprises often encounter. Therefore, UNI is proud to be the first enterprise in Vietnam specializing in providing customs consultancy services and post-customs clearance data inspection, ensuring "specializing in customs" superior to the companies providing services in the market today.',
+        '베트남에서는 복잡하고 다양한 관세 문제들이 발생하고 있습니다. 일반 물류회사 수준에서는 관세문제를 정확히 대응하기 어려운 경우가 많습니다. UNI는 "베트남 관세 전문가"로서 베트남 수출입 통관 및 세관감사 대응 등 모든 관세 실무를 정확하게 처리하고 있습니다.',
     },
   },
 };
