d98a9917665d2f35e2b6771c11ab66a3aaf2346a
boy2balls <nguyendanghoaa@gmail.com>
2021-12-16 11:14:57 +0700

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {footerCons} from "./constants/footerCons";
+import { footerCons } from "./constants/footerCons";
 function Footer({ lang }) {
   return (
     <footer id="footer">
@@ -10,7 +10,8 @@ function Footer({ lang }) {
               <h4>{footerCons[lang].contact}</h4>
               <p>
                 <strong>{footerCons[lang].address1}: </strong>
-                {footerCons[lang].address1_}<br />
+                {footerCons[lang].address1_}
+                <br />
                 <strong>{footerCons[lang].address2}: </strong>
                 {footerCons[lang].address2_} <br />
                 <strong>{footerCons[lang].phone}: </strong>
@@ -23,14 +24,10 @@ function Footer({ lang }) {
             </div>
             <div className="col-lg-3 col-md-6 footer-newsletter">
               <h4>{footerCons[lang].newletter}</h4>
-              <p>
-                Tamen quem nulla quae legam multos aute sint culpa legam noster
-                magna veniam enim veniam illum dolore legam minim quorum culpa
-                amet magna export quem marada parida nodela caramase seza.
-              </p>
+              <p>{footerCons[lang].newlettercontent}</p>
               <form action="" method="post">
-                <input type="email" name="email" placeholder="Email"/>
-                <input type="submit" value="Subscribe" />
+                <input type="email" name="email" placeholder="Email" />
+                <input type="submit" value={footerCons[lang].sub} />
               </form>
             </div>
           </div>
--- src/components/constants/footerCons.js
+++ src/components/constants/footerCons.js
@@ -6,7 +6,9 @@ export const footerCons = {
     address2:'Address 2',
     address2_:'220 Hoang Ngan, Trung Hoa Ward, Cau Giay District, Hanoi',
     phone:'Phone',
-    newletter:'OUR NEWSLETTER'
+    newletter:'OUR NEWSLETTER',
+    newlettercontent:'Please subscribe to receive news, documents and analysis related to your industry and business biweekly from UNI.',
+    sub:'Subscribe'
   },
   VI: {
     contact:'LIÊN HỆ',
@@ -15,13 +17,18 @@ export const footerCons = {
     address2:'Địa chỉ 2',
     address2_:'220 Hoàng Ngân, Trung Hòa, Cầu Giấy, Hà Nội',
     phone:'SĐT',
-    newletter:'Đăng ký nhận bản tin'
+    newletter:'Đăng ký nhận bản tin',
+    newlettercontent:'Quý khách hàng vui lòng nhấn đăng ký để nhận tin tức, văn bản và các phân tích liên quan tới ngành sản xuất kinh doanh của mình hàng tuần từ UNI.',
+    sub:'Đăng ký'
+
   },
   KO: {
     contact:'CONTACT US',
     address1:'Saigon Royal Residence,34-35 Ben Van Don, Ward 12, District 4, Ho Chi Minh City',
     address2:'220 Hoang Ngan, Trung Hoa Ward, Cau Giay District, Hanoi',
     phone:'sdt',
-    newletter:'OUR NEWSLETTER'
+    newletter:'OUR NEWSLETTER',
+    newlettercontent:'Quý khách hàng vui lòng nhấn đăng ký để nhận tin tức, văn bản và các phân tích liên quan tới ngành sản xuất kinh doanh của mình hàng tuần từ UNI.',
+    sub:'Đăng ký'
   },
 };
